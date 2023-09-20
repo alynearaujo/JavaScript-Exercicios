14828f3ca3976f1e997878d8df0c5e0ba9135914
Alyne Araujo <alyne.arauujo@gmail.com>
2023-09-19 21:11:15 -0300

    exercícios de js

---
+++ ex001/ex1_2.js
@@ -0,0 +1,9 @@
+// Operações envolvendo strings e números
+
+const a = "20"          // string
+const b = a * 2         // b = 40
+const c = a / 2         // c = 10
+const d = a - 2         // d = 18
+const e = a + 2         // e = 202
+
+console.log(`e: ${e}`)
---
+++ ex001/ex1_3.js
@@ -0,0 +1,10 @@
+// Tipos de variáveis
+const fruta = "Banana"
+const preco = 3.50
+const levar = true
+let novoValor
+
+console.log(typeof fruta)
+console.log(typeof preco)
+console.log(typeof levar)
+console.log(typeof novoValor)
