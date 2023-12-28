51e40a628fca8937150fbe504029c3720f93687f
Alyne Araujo <alyne.arauujo@gmail.com>
2023-12-27 22:26:57 -0300

    learning about Math

---
+++ ex001/ex1_13.js
@@ -0,0 +1,10 @@
+let num1 = 10.589012310012
+let num2 = 2.5
+
+// console.log(num1.toString() + num2)
+// num1 = num1.toString()
+// console.log(num1.toString(2)) -> converte um número para uma string
+// console.log(num1.toFixed(2)) -> arredonda um número para um número específico de casas decimais
+// console.log(Number.isInteger(num1)) -> verifica se um valor é inteiro.
+// let temp = num1 + 'Olá'
+// console.log(Number.isNaN(temp)) -> verifica se um valor é NaN (not a number) retornando true ou false
---
+++ ex001/ex1_14.js
@@ -0,0 +1,7 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1); // Arredonda para o número inteiro anterior.
+// let num2 = Math.ceil(num1) // Arredonda para o próximo número inteiro.
+// let num2 = Math.round(num1) // Arredonda para o inteiro mais próximo.
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // encontra o maior valor entre dois ou mais números
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // encontra o menor valor entre dois ou mais números
+// console.log(Math.random()); // Retorna um número decimal aleatório entre 0 (inclusive) e 1 (exclusive).
