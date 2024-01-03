b09c0478df1a5ceb294ccaf5d60442131db878c1
Alyne Araujo <alyne.arauujo@gmail.com>
2024-01-02 23:38:23 -0300

    learning about array

---
+++ ex001/ex1_15.js
@@ -0,0 +1,15 @@
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+// console.log(alunos[0])
+// console.log(alunos.length)  // tamanho do array
+// alunos.push('Luiza')  // adiciona um elemento no final de um array
+// alunos[3] = 'Flavio' // adiciona um elemento no índice indicado (substitui o elemento caso ja exista no indice)
+// alunos.unshift('Dina') // adiciona um elemento no inicio do array
+//const removido = alunos.pop() //salva na variavel o elemento removido
+//alunos.pop() // remove o ultimo elemento
+// alunos.shift() // remove o elemento do inicio (também é possível guardar numa variável)
+// delete alunos [1] // deleta um elemento sem alterar o índice 
+
+
+console.log(alunos)
