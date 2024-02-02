a69b677821081ebec31b07aefea91f518f57dea8
Alyne Araujo <alyne.arauujo@gmail.com>
2024-02-01 22:25:32 -0300

    operador &&

---
+++ ex002/index.js
@@ -0,0 +1,11 @@
+/* operador && (and) */
+let idade = 25;
+let possuiCNH = true;
+
+if (idade > 18 && possuiCNH) {
+    console.log("Pode dirigir.")
+} else {
+    console.log("Não pode dirigir.")
+}
+
+/* operador || (OU) */
