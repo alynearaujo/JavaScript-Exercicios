575d17269f3e7c3c0f2e181806e2684377669d65
Alyne Araujo <alyne.arauujo@gmail.com>
2024-02-01 22:47:11 -0300

    operador !

--- ex002/index.js
+++ ex002/index.js
@@ -8,11 +8,20 @@ if (idade > 18 && possuiCNH) {
     console.log("Não pode dirigir.")
 }
 
-/* operador || (OU) */
+/* operador || (OR) */
 let diaSemana = "segunda";
 
 if (diaSemana === "sábado" || diaSemana === "domingo") {
     console.log("É fim de semana.")
 } else { 
     console.log("É dia útil.")
+}
+
+/* operador ! (NOT) */
+let chovendo = false;
+
+if (!chovendo) {
+    console.log("Não precisa de guarda-chuva.")
+} else {
+    console.log("Leve um guarda-chuva.")
 }
